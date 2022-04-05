f1bfb382754b87ab26ccd0b46fb62d8e25526ff4
Ryan-Grabham <gra18001287@btc.ac.uk>
2022-04-05 14:46:56 +0100

    collapsible Nav

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -7,18 +7,18 @@ function Header() {
         <div>
             <Navbar className="navbar" expand="lg" bg="" variant="dark">
                 <Container>
-                    <Navbar.Brand className="justify-content" href="#home">The Cloakroom Cafe <Image src="/images/coffee-beans.png" width="30rem" /></Navbar.Brand>
+                    <Navbar.Brand className="justify-content" href="/">The Cloakroom Cafe <Image src="/images/coffee-beans.png" width="30rem" /></Navbar.Brand>
                     <Navbar.Toggle aria-controls="basic-navbar-nav" />
                     <Navbar.Collapse id="basic-navbar-nav">
                         <Nav className="me-auto">
-                            <Nav.Link href="#features">Coffee Club</Nav.Link>
-                            <Nav.Link href="#pricing">Order Online</Nav.Link>
-                            <Nav.Link href="#features">Reviews</Nav.Link>
-                            <Nav.Link href="#pricing">About</Nav.Link>
+                            <Nav.Link href="#club">Coffee Club</Nav.Link>
+                            <Nav.Link href="#order">Order Online</Nav.Link>
+                            <Nav.Link href="#reviews">Reviews</Nav.Link>
+                            <Nav.Link href="#about">About</Nav.Link>
                         </Nav>
                         <Nav className="justify-content-end">
-                            <Nav.Link href="#home"><img src="/images/location-pin.png" width="30" height="30" alt="location" /></Nav.Link>
-                            <Nav.Link href="#home"><img src="/images/user.png" width="30" height="30" alt="account" /></Nav.Link>
+                            <Nav.Link href="#location"><img src="/images/location-pin.png" width="30" height="30" alt="location" /></Nav.Link>
+                            <Nav.Link href="#account"><img src="/images/user.png" width="30" height="30" alt="account" /></Nav.Link>
                         </Nav>
                     </Navbar.Collapse>
                 </Container>
