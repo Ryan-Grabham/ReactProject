b90914527df082fa5cc71871a02e1e59f5f7c413
Ryan-Grabham <gra18001287@btc.ac.uk>
2022-04-05 14:43:04 +0100

    collapsible Nav

--- src/Screens/AddProductScreen.jsx
+++ src/Screens/AddProductScreen.jsx
@@ -2,83 +2,102 @@ import React, {useState, useEffect} from 'react';
 import FormContainer from '../Components/FormContainer';
 import {Form, Button} from 'react-bootstrap'
 import Loader from '../Components/Loader'
+import { render } from '@testing-library/react';
 
 
 function AddProductScreen() {
 
-    const submitHandler = async () => {
-        SetPosting(true)
-        var input = document.getElementById('upload')
-        var formData = new FormData(input)
-        var data = {}
-        formData.forEach((value, key) => {
-            data[key] = value
-        })
+    const [price, setPrice] = useState(0)
+    const [name, setName] = useState('')
+    const [desc, setDesc] = useState('')
+    const [image, setImage] = useState('')
 
-        let response = await fetch("https://localhost:7214/api/products",
+    const submitHandler = async () =>{
+
+        var file = document.getElementById("image")['files'][0]
+
+        var reader = new FileReader()
+        reader.onloadend = () => {
+            setImage(reader.result)
+        }
+        reader.readAsDataURL(file)
+
+        console.log(image)
+        await fetch('https://localhost:7214/api/products', 
         {
-            method: "POST",
-            body: data,
+            method: 'POST',
             headers: {
-                "Content-Type": "multipart/form-data"
-            }
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify({
+                Name: name,
+                Price: price,
+                Description: desc,
+                Image: image
+            })
         })
-
-        console.log(response)
-
+       .then(response => response.json())
+       .catch((error) => {
+            console.error('error', error)
+       })
+        
 
     }
 
-    return <div>
 
-        <h1 className="py-3 text-center">Add Product</h1>
-        <FormContainer>
+  return <div>
 
-            <Form Id="upload">
+    <h1 className="py-3 text-center">Add Product</h1>
+    <FormContainer>
 
-                <Form.Group className="mb-3" controlId="name">
-                    <Form.Label>Product Name</Form.Label>
-                    <Form.Control
-                        type="text"
-                        placeholder="Product Name..."
-                    />
-                </Form.Group>
+    <Form id="upload" encType="multipart/form-data">
 
-                <Form.Group className="mb-3" controlId="price">
-                    <Form.Label>Product Price</Form.Label>
-                    <Form.Control
-                        type="number"
-                        placeholder="Product Price (£)..."
-                    />
-                </Form.Group>
+        <Form.Group className="mb-3" controlId="name">
+            <Form.Label>Product Name</Form.Label>
+            <Form.Control 
+                type="text" 
+                placeholder="Product Name..." 
+                onChange={(e) => setName(e.target.value)}
+            />
+        </Form.Group>
 
-                <Form.Group className="mb-3" controlId="description">
-                    <Form.Label>Product Description</Form.Label>
-                    <Form.Control
-                        type="text"
-                        placeholder="Product Description..."
-                        onChange={(e) => SetDescription(e.target.value)}
-                    />
-                </Form.Group>
+        <Form.Group className="mb-3" controlId="price">
+            <Form.Label>Product Price</Form.Label>
+            <Form.Control 
+                type="number" 
+                placeholder="Product Price (£)..." 
+                onChange={(e) => setPrice(e.target.value)}
+            />
+        </Form.Group>
 
-                <Form.Group controlId="image" className="mb-3">
-                    <Form.Label>Product Image</Form.Label>
-                    <Form.Control type="file" />
-                </Form.Group>
+        <Form.Group className="mb-3" controlId="description">
+            <Form.Label>Product Description</Form.Label>
+            <Form.Control 
+                type="text" 
+                placeholder="Product Description..." 
+                onChange={(e) => setDesc(e.target.value)}
+            />
+        </Form.Group>
 
-                <Button
-                    variant="primary"
-                    className="w-100"
-                    onClick={submitHandler}
-                >  {posting ? <Loader /> : (<div>Add Product</div>)}   </Button>
-            </Form>
+        <Form.Group controlId="image" className="mb-3">
+            <Form.Label>Product Image</Form.Label>
+            <Form.Control type="file" />
+        </Form.Group>
+        
+        <Button 
+            variant="primary"
+            className="w-100"
+            onClick={submitHandler}
+        >  <div>Add Product</div>
+        </Button>
+    </Form>
 
 
 
-        </FormContainer>
+    </FormContainer>
 
 
-    </div>;
+  </div>;
 }
 
 export default AddProductScreen;
--- src/Screens/HomeScreen.jsx
+++ src/Screens/HomeScreen.jsx
@@ -1,12 +1,21 @@
 import React from 'react'
-import {Button} from 'react-bootstrap'
+import {Button, Image, Row, Col, Container} from 'react-bootstrap'
 import {Link} from 'react-router-dom'
 
 function HomeScreen() {
     return(
-        <dvi>
-            <h1>coffee</h1>
-        </dvi>
+        <div>
+            <Container className="indexCard" >
+            <Row className="my-5">
+                <Col >
+                <h2>Coffee</h2>
+                </Col>
+                <Col className="px-0">
+                <Image  fluid src="/images/CoffeePour.jpg"/>
+                </Col>
+            </Row>
+            </Container>
+        </div>
     )
 }
 
