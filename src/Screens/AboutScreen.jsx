2dcc4ef2cdb476ab72dcc0f731fd0ef3eb4c04bd
Ryan Grabham <Gra18001287@btc.ac.uk>
2022-04-11 14:43:06 +0100

    add products

--- src/Screens/AboutScreen.jsx
+++ src/Screens/AboutScreen.jsx
@@ -1,9 +1,35 @@
 import React from 'react'
+import { Button, Image, Row, Col, Container } from 'react-bootstrap'
 
 function AboutScreen() {
     return (
         <div>
-            <h1>About us!</h1>
+            <Container className="my-5 text-center">
+                <h2>About us!</h2>
+            </Container>
+            <Container className="indexCard" >
+                <Row className="my-5 text-center">
+                    <Col className="py-5" >
+                        <h3>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam venenatis finibus aliquet. Proin quis gravida felis, eget mattis nisl. Mauris mollis nulla massa, at commodo eros fermentum quis. Sed pulvinar rutrum erat quis congue.</h3>
+
+                    </Col>
+                    <Col className="px-0">
+                        <Image fluid src="/images/CoffeePour.jpg" />
+                    </Col>
+                </Row>
+            </Container>
+            <Container className="indexCard" >
+                <Row className="my-5 text-center">
+                    <Col className="px-0">
+                        <Image fluid src="/images/CoffeePour.jpg" />
+                    </Col>
+                    <Col className="py-5">
+                        <h3>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam venenatis finibus aliquet. Proin quis gravida felis, eget mattis nisl. Mauris mollis nulla massa, at commodo eros fermentum quis. Sed pulvinar rutrum erat quis congue.</h3>
+                    </Col>
+
+                </Row>
+            </Container>
+
         </div>
     )
 }
